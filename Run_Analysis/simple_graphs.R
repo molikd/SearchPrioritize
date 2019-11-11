ad1df57f1a22dc262ce653d2685ec3766be9c7c7
dave <dave@d3fy.net>
2019-11-11 17:06:51 -0500

    adding non_zeros

--- Run_Analysis/simple_graphs.R
+++ Run_Analysis/simple_graphs.R
@@ -1,9 +1,13 @@
+library(lattice)
+
 #sp_google_results <- read.table("results_1_Insecta.txt", as.is=TRUE, header=FALSE)
 #sp_google_results.nz <- sp_google_results[sp_google_results$V3!=0,]
 
 sp_genome_results <- read.table("results_2_Insecta.txt", as.is=TRUE, header=FALSE)
 sp_genome_results.nz <- sp_genome_results[sp_genome_results$V3!=0,]
-plot(density(sp_genome_results.nz$V3))
+
+plot(density(sp_genome_results.nz$V3), main="Density of Non-Zero Genomes")
+polygon(density(sp_genome_results.nz$V3), col="#82b446") 
 
 plot(sp_genome_results.nz[order(-sp_genome_results.nz$V3),]$V3)
 
@@ -11,10 +15,20 @@ sp_pubmed_results <- read.table("results_3_Insecta.txt", as.is=TRUE, header=FALS
 sp_pubmed_results.nz <- sp_pubmed_results[sp_pubmed_results$V3!=0,]
 plot(density(sp_pubmed_results.nz$V3))
 
+
+
+
 sp_pubmed_results.nz.lt20 <- sp_pubmed_results.nz[sp_pubmed_results.nz$V3<20,]
-plot(density(sp_pubmed_results.nz.lt20$V3))
+
+pubmed.density <- density(sp_pubmed_results.nz.lt20$V3, bw=.5)
+plot(pubmed.density, main="Density of Non-Zero, Less Than 20 Pubmed")
+polygon(pubmed.density, col="steelblue") 
 
 mean(sp_pubmed_results$V3)
 mean(sp_pubmed_results[sp_pubmed_results$V3!=0,]$V3)
 
-plot(sp_pubmed_results.nz[order(-sp_pubmed_results.nz$V3),]$V3)
+ordered_sp_pubmed_results.nz <- sp_pubmed_results.nz[order(-sp_pubmed_results.nz$V3),]
+
+plot(ordered_sp_pubmed_results.nz$V3, type="h", ylab="Number Hits", ylim=c(0,100), col="steelblue", main="Number of Pubmed Hits")
+
+plot(ordered_sp_pubmed_results.nz$V3, pch=16 , col= ifelse(ordered_sp_pubmed_results.nz$V2 %in% sp_genome_results.nz$V2, "steelblue", "#82b446"), main="Number of Pubmed Hits, Colored by Genome", ylab="Number Hits")
